dc9f089d4746a0b7fb37894f00b80869360541ac
Huynh Van Huy <huynhhuy190804@gmail.com>
2026-03-26 13:24:21 +0700

    Fix frontend data parsing and backend CORS

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -14,20 +14,27 @@ const allowedOrigins = [
   "https://app.com",
   "https://project-management-d174.vercel.app",
   "http://localhost:3000",
+  "http://localhost:5173",
+  "http://localhost:4173"
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (
+        !origin || 
+        origin.includes("localhost") || 
+        origin.includes("127.0.0.1") || 
+        origin.endsWith(".vercel.app") || 
+        allowedOrigins.includes(origin)
+      ) {
         callback(null, true);
       } else {
         callback(new Error("Origin not allowed"));
       }
     },
-
     credentials: true,
-  }),
+  })
 );
 // Middleware quan trọng: Giúp Express hiểu được dữ liệu JSON
 // Nếu thiếu dòng này, req.body sẽ bị undefined
